296a6de35e2501dcd6c9bf1f4e43cde4a3b2917c
sebaswn <sebastiannee@yahoo.com>
2015-12-17 22:03:49 +0100

    added animation to space bar when clicked and uncliked. THinking about changing the design of it.

--- app/assets/javascripts/autoScroll.js
+++ app/assets/javascripts/autoScroll.js
@@ -2,13 +2,17 @@ var ani = false
 
 $(window).keypress(function(event){
   event.preventDefault();
+ 
 
   if ((event.keyCode === 0 && ani === true) || (event.keyCode === 32 && ani === true)) {
     ani = false
+    $(".spaceBarJpg").css("opacity", "1")
+
     $('.parallax').stop()
   }else if ((event.keyCode === 0 && ani === false) || (event.keyCode === 32 && ani === false)) {
     ani = true
     
+    $(".spaceBarJpg").css("opacity", "0.6")
     $('.parallax').stop().animate({
       scrollTop: $(".parallax")[0].scrollHeight
     }, (($(".parallax")[0].scrollHeight-$(".parallax").scrollTop())/1000)*1000)
