fab5cc47eaece53c465a683d14ef2c836e641428
sebaswn <sebastiannee@yahoo.com>
2015-12-21 22:46:10 +0100

    added new planets

--- app/assets/javascripts/autoScroll.js
+++ app/assets/javascripts/autoScroll.js
@@ -7,12 +7,17 @@ $(window).keypress(function(event){
   if ((event.keyCode === 0 && ani === true) || (event.keyCode === 32 && ani === true)) {
     ani = false
     $(".spaceBarJpg").css("opacity", "1")
+    $(".spaceName").css("color", "black")
+
 
     $('.parallax').stop()
   }else if ((event.keyCode === 0 && ani === false) || (event.keyCode === 32 && ani === false)) {
     ani = true
     
     $(".spaceBarJpg").css("opacity", "0.6")
+    $(".spaceName").css("color", "white")
+
+
     $('.parallax').stop().animate({
       scrollTop: $(".parallax")[0].scrollHeight
     }, (($(".parallax")[0].scrollHeight-$(".parallax").scrollTop())/1000)*1000)
